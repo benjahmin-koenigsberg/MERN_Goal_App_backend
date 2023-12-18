f9f76ff1e927447f9842e574c857a24fb80e68cb
Benjahmin Koenigsberg <benjahmin.lakin@gmail.com>
2023-12-18 18:48:23 -0500

    Update server.js

--- server.js
+++ server.js
@@ -15,7 +15,9 @@ const app = express()
 
 app.use(express.json())
 app.use(express.urlencoded({extended:false}))
-app.use(cors())
+// app.use(cors())
+app.use(cors({ origin: "*", credentials: true }))
+
 
 app.use('/api/goals', require('./routes/goalRoutes.js'))
 
