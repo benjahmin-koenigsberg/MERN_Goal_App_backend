3051c4a9aefe5f901f7ad396d1d631208105550e
Benjahmin Koenigsberg <benjahmin.lakin@gmail.com>
2023-12-18 17:35:35 -0500

    Add files via upload

---
+++ server.js
@@ -0,0 +1,39 @@
+const express = require('express')
+require('dotenv').config()
+const { errorHandling } = require('./middleware/errorMiddleware.js')
+const connectDB = require('./config/db.js')
+const colors = require('colors')
+const path = require('path')
+const cors = require('cors')
+
+const port = process.env.PORT || 5000
+
+
+connectDB()
+
+const app = express()
+
+app.use(express.json())
+app.use(express.urlencoded({extended:false}))
+app.use(cors())
+
+app.use('/api/goals', require('./routes/goalRoutes.js'))
+
+app.use('/api/users', require('./routes/userRoutes.js'))
+
+
+
+//server front end
+if(process.env.NODE_ENV === 'production'){
+    app.use(express.static(path.join(__dirname, '../frontend/build')))
+
+    app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, '../', 'frontend', 'build', 'index.html')) )
+} else {
+    app.get('/', (req, res) => res.send('Please set to production'))
+}
+
+app.use(errorHandling)
+
+app.listen( port, ()=>{
+    console.log(`server startded on ${port} 🚀...`)
+})
