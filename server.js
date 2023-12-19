e8c30c2708c6a6d663128154c02fb5346d164ff4
Benjahmin Koenigsberg <benjahmin.lakin@gmail.com>
2023-12-18 19:20:02 -0500

    Update server.js
    moves cors above json middleware

--- server.js
+++ server.js
@@ -13,10 +13,11 @@ connectDB()
 
 const app = express()
 
+app.use(cors())
+//app.use(cors({ origin: "*", credentials: true }))
+
 app.use(express.json())
 app.use(express.urlencoded({extended:false}))
-// app.use(cors())
-app.use(cors({ origin: "*", credentials: true }))
 
 
 app.use('/api/goals', require('./routes/goalRoutes.js'))
